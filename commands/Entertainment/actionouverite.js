8c861725bb61bec64bcc0dc373c8441e439b4f9f
Romar1 <35073934+Romar159@users.noreply.github.com>
2023-02-11 20:01:09 +0100

    Améliorations diverses du action ou vérité
    - Design amélioré
    - Corrections de bugs
    - Ajout de fonctionnalités

--- commands/Entertainment/actionouverite.js
+++ commands/Entertainment/actionouverite.js
@@ -1,8 +1,8 @@
 const { ActionRowBuilder, ButtonBuilder } = require("@discordjs/builders");
-const { ButtonStyle } = require("discord.js");
+const { ButtonStyle, EmbedBuilder } = require("discord.js");
 const levenshtein = require('js-levenshtein');
 
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (client, message, args, settings, dbUser, command, mentionnedUser, user) => {
     
     client.logCommandExecution(message, this);
 
@@ -18,21 +18,36 @@ module.exports.run = async (client, message, args) => {
         }
     }
 
+
     var obj = await client.getRandomAov(cat);
     // message.channel.send("data: " + obj.texte);
 
-    const filter = i => (i.customId === 'act' || i.customId === 'vrt');
+    const embedaov = new EmbedBuilder()
+        .setAuthor({name: user.username, iconURL: user.displayAvatarURL()})
+        .setDescription(obj.texte);
+
+    if(cat == "action") embedaov.setColor('D53B3E');
+    else if(cat == "vérité") embedaov.setColor('359553');
+
+   
+
+    const filter = i => (i.customId === 'act' || i.customId === 'vrt' || i.customId === 'ran');
     const collector = message.channel.createMessageComponentCollector({ filter, time: 300000 }); //5 minutes
         const row = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
                 .setCustomId(`act`)
                 .setLabel('Action')
-                .setStyle(ButtonStyle.Primary),
+                .setStyle(ButtonStyle.Danger),
             new ButtonBuilder()
                 .setCustomId(`vrt`)
                 .setLabel('Vérité')
+                .setStyle(ButtonStyle.Success),
+            new ButtonBuilder()
+                .setCustomId(`ran`)
+                .setLabel('Aléatoire')
                 .setStyle(ButtonStyle.Primary)
+                
         ); 
 
     await collector.on('collect', async i => { 
@@ -41,17 +56,21 @@ module.exports.run = async (client, message, args) => {
             if (i.customId === `act`) { 
                 await i.deferUpdate();
                 collector.stop();
-                this.run(client, message, ["action"]);
+                this.run(client, msg, ["action"], settings, dbUser, command, mentionnedUser, i.user);
 
             } else if (i.customId === `vrt`) {
                 await i.deferUpdate();
                 collector.stop();
-                this.run(client, message, ["vérité"]);
+                this.run(client, msg, ["vérité"], settings, dbUser, command, mentionnedUser, i.user);
+            } else if (i.customId === `ran`) {
+                await i.deferUpdate();
+                collector.stop();
+                this.run(client, msg, [""], settings, dbUser, command, mentionnedUser, i.user);
             }
         }
     });
 
-    await message.reply({content: obj.texte, components: [row]});
+    let msg = await message.reply({embeds:[embedaov], components: [row]});
 
 }
 
